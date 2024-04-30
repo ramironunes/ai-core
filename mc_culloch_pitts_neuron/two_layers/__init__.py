a11bbc9057cb46cf63f50afaf4fbd2da5e59ddde
ramiro.nunes <ramronunes@gmail.com>
2024-04-30 07:35:23 -0300

    Added "McCullochPittsNeuron for two layers and mlp"

---
+++ mc_culloch_pitts_neuron/two_layers/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: Ramiro Luiz Nunes
+# @Date:   2024-03-25 21:33:26
+# @Last Modified by:   Ramiro Luiz Nunes
+# @Last Modified time: 2024-03-25 21:33:30
+
---
+++ mlp/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# @Author: Ramiro Luiz Nunes
+# @Date:   2024-04-02 07:13:33
+# @Last Modified by:   Ramiro Luiz Nunes
+# @Last Modified time: 2024-04-02 07:13:39
+
